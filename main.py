ab04be8ab3c8dfea280b50fe0e742f6f6b893ec3
Mason Kim <sungjinkimm@gmail.com>
2021-12-03 23:54:18 -0500

    csv file added and changed line graph to horizontal bar graph

--- main.py
+++ main.py
@@ -1,20 +1,37 @@
 from bokeh.plotting import figure, output_file, show
+import pandas
 
-x = [1, 2, 3, 4, 5]
-y = [4, 6, 2, 4, 3]
+# x = [1, 2, 3, 4, 5]
+# y = [4, 6, 2, 4, 3]
+
+# Read in csv
+df = pandas.read_csv('cars.csv')
+
+car = df['Car']
+hp = df['Horsepower']
 
 output_file('index.html')
 
 # Add plot
-
 p = figure(
-    title='Simple Example',
-    x_axis_label='X Axis',
-    y_axis_label='Y Axis'
+    y_range=car,
+    plot_width=800,
+    plot_height=600,
+    title='Cars With Top HorsePower',
+    x_axis_label='Horsepower',
+    # y_axis_label='Y Axis'
+    tools=''
 )
 
 # Render glyph
-p.line(x, y, legend_label='Test', line_width=2)
+p.hbar(
+    y=car,
+    right=hp,
+    left=0,
+    height=0.4,
+    color='orange',
+    fill_alpha=0.5
+)
 
 # show
 show(p)
